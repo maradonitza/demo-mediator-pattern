0dc4685ad8a79a042bb00acd98ab9af7c4a9ed87
maradonitza <maradonitza@gmail.com>
2020-09-07 11:57:58 +0300

    Create ASP.NET Core web app. Add MediaR and related DI package. Add EntityFrameworkCore.InMemory package. Add MyDatabaseContext class. Changes to Startup.cs.

---
+++ MediatRDemo/Data/MyDatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediatRDemo.Data
+{
+    public class MyDatabaseContext : DbContext
+    {
+        public MyDatabaseContext(DbContextOptions<MyDatabaseContext> options) : base(options)
+        {
+        }
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().HasData(
+                new Person { Id = 1, Nickname = "Max Amini" },
+                new Person { Id = 2, Nickname = "Maz Jobrani" });
+        }
+    }
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Nickname { get; set; }
+    }
+}
