3809c5b1bd5473523ff7206989a56a667fc284e5
Ana-MariaN.Dobre <Ana-MariaN.Dobre@endava.com>
2020-09-07 11:40:00 +0300

    delegate person creation to mediator (variation)

--- MediatorDemo/ConcreteMediator.cs
+++ MediatorDemo/ConcreteMediator.cs
@@ -13,6 +13,15 @@ namespace MediatorDemo
             persons.Add(person);
             person.SetMediator(this);
         }
+        public T CreatePerson<T>(string nickname) where T : Person //, new()
+        {
+            //var c = new T();
+            //c.Nickname = nickname;
+            var person = (T)Activator.CreateInstance(typeof(T), new object[] { nickname });
+            Register(person);
+            return person;
+        }
+
         public override void Send(Person person, string notification)
         {
             this.persons.Where(p => p != person).ToList().ForEach(p => p.HandleNotification(person.Nickname, notification));
--- MediatorDemo/Program.cs
+++ MediatorDemo/Program.cs
@@ -7,11 +7,10 @@ namespace MediatorDemo
         static void Main(string[] args)
         {
             var mediator = new ConcreteMediator();
-            var comedian1 = new Comedian("Max Amini"); 
-            var comedian2 = new Comedian("Maz Jobrani");
-
-            mediator.Register(comedian1);
-            mediator.Register(comedian2);
+            
+            var comedian1 = mediator.CreatePerson<Comedian>("Max Amini");
+            var comedian2 = mediator.CreatePerson<Comedian>("Maz Jobrani");
+            var comedian3 = mediator.CreatePerson<Comedian>("Carrey");
 
             comedian2.Send("Hey, I made a video making fun of corporate jargon.");
             comedian1.Send("I see, dealing with cross-cutting concerns!");
